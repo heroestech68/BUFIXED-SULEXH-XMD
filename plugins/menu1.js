51e0409daa522e76d6158b6ba1926a799ac5ef8c
heroestech68 <heroesytech6816@gmail.com>
2025-09-02 21:42:14 +0300

    Update menu1.js

--- plugins/menu1.js
+++ plugins/menu1.js
@@ -37,16 +37,16 @@ ezra({ nomCom: "menu1", categorie: "Menu" }, async (dest, zk, commandeOptions) =
 ┃⊛│◆ 𝙼𝚘𝚍𝚎 : *${mode}*
 ┃⊛│◆ 𝚁𝚊𝚖  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
 ┃⊛│◆ 𝙳𝚊𝚝𝚎  : *${date}*
-┃⊛│◆ made::::in Kenya/Eldoret 
+┃⊛│◆ made::::in Kenya/Tanariver 
 ┃⊛│◆ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
-┃⊛│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : JEEPERS CREEPER-XMD TECH
+┃⊛│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : BUGFIXED SULEXH-XMD TECH
 ┃⊛│◆ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
-┃⊛│◆ 𝚃𝚑𝚎𝚖𝚎 : TOXIC LOVER DEVELOPER https://whatsapp.com/channel/0029VawCel7GOj9ktLjkxQ3g
+┃⊛│◆ 𝚃𝚑𝚎𝚖𝚎 : BUGFIXED SULEXH DEVELOPER WHATSAPP CHANNEL https://whatsapp.com/channel/0029VbAD3222f3EIZyXe6w16
 ┃⊛└────••••────➻
 ╰─━━━━══──══━━━❂\n${readmore}
 `;
 
-    let menuMsg = `JEEPERS CREEPER-XMD 𝙲𝚖𝚍`;
+    let menuMsg = `BUGFIXED SULEXH-XMD 𝙲𝚖𝚍`;
     
     for (const cat in coms) {
         menuMsg += `
@@ -63,7 +63,7 @@ ezra({ nomCom: "menu1", categorie: "Menu" }, async (dest, zk, commandeOptions) =
     }
     
     menuMsg += `
-> Made By sir bravin ᴛᴇᴄʜ\n`;
+> Made By bugfixed sulexh ᴛᴇᴄʜ\n`;
 
     try {
         const senderName = nomAuteurMessage || message.from;  // Use correct variable for sender name
@@ -72,10 +72,10 @@ ezra({ nomCom: "menu1", categorie: "Menu" }, async (dest, zk, commandeOptions) =
             contextInfo: {
                 mentionedJid: [senderName],
                 externalAdReply: {
-                    title: "JEEPERS CREEPER-XMD MENU LIST",
+                    title: "BUGFIXED SULEXH-XMD MENU LIST",
                     body: "Dont worry  I have more tap to follow",
-                    thumbnailUrl: "https://files.catbox.moe/uoxzsc.jpg",
-                    sourceUrl: "https://whatsapp.com/channel/0029VbAoW2dAInPcTs6Fmg1o",
+                    thumbnailUrl: "https://files.catbox.moe/x6k68g.png",
+                    sourceUrl: "https://whatsapp.com/channel/0029VbAD3222f3EIZyXe6w16",
                     mediaType: 1,
                     renderLargerThumbnail: true
                 }
