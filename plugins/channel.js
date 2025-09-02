2c8bacd0ea4671d71a49392b4ffa038f95320510
heroestech68 <heroesytech6816@gmail.com>
2025-09-02 21:18:45 +0300

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -4,10 +4,10 @@ const { ezra } = require("../fredi/ezra");
 
 ezra({ nomCom: "channel", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle JEEPERS CREEPER-XMD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let z = 'Salut je m\'appelle BUGFIXED SULEXH-XMD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
     let d = ' developpé par *sir bravin*';
     let varmess = z + d;
-    var lien = 'https://whatsapp.com/channel/0029VawCel7GOj9ktLjkxQ3g';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    var lien = 'https://whatsapp.com/channel/0029VbAD3222f3EIZyXe6w16';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
 });
 
@@ -23,9 +23,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *JEEPERS CREEPER-XMD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *sir bravin*'
+   let z ='Salut je m\'appelle *BUGFIXED SULEXH-XMD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *bugfixed sulexh*'
       let varmess=z+d
-      var img='https://whatsapp.com/channel/0029VbAoW2dAInPcTs6Fmg1o'
+      var img='https://whatsapp.com/channel/0029VbAD3222f3EIZyXe6w16'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
